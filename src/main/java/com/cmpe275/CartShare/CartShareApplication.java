1a9bb114fcff66bede4fb7b424f248b5f03b4022
Alok Goyal <alok.goyal@sjsu.edu>
2020-04-22 00:16:52 -0700

    spring boot project setup

---
+++ src/main/java/com/cmpe275/CartShare/CartShareApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe275.CartShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmpe275/CartShare/CartShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpe275.CartShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
