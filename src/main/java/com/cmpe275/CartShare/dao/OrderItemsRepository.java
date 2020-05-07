cac4f2719b5387ae5babfb8ea05f4aa9b3fd3a96
Farhan <farhanbhoraniya@gmail.com>
2020-05-07 15:40:02 -0700

    Added get user orders

--- src/main/java/com/cmpe275/CartShare/dao/OrderItemsRepository.java
+++ src/main/java/com/cmpe275/CartShare/dao/OrderItemsRepository.java
@@ -1,12 +1,16 @@
 package com.cmpe275.CartShare.dao;
 
+import java.util.List;
+
 import org.hibernate.sql.Insert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cmpe275.CartShare.model.Order;
 import com.cmpe275.CartShare.model.OrderItems;
 
 public interface OrderItemsRepository extends JpaRepository<OrderItems, Integer>{
 	public OrderItems save(OrderItems orderItem);
 	public OrderItems saveAndFlush(OrderItems orderItem);
+	public List<OrderItems> findByOrder(Order order);
 
 }
