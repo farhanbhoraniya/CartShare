46deaafaf8a3b291ad5e80cafb02455874f219af
shubhamzingh <shubham.zingh@gmail.com>
2020-05-07 21:03:30 -0700

    get linked orders at pickup

--- src/main/java/com/cmpe275/CartShare/contollers/PickupController.java
+++ src/main/java/com/cmpe275/CartShare/contollers/PickupController.java
@@ -1,8 +1,8 @@
 package com.cmpe275.CartShare.contollers;
 
-import com.cmpe275.CartShare.model.CartItem;
-import com.cmpe275.CartShare.model.Order;
-import com.cmpe275.CartShare.model.User;
+import com.cmpe275.CartShare.dao.LinkedOrdersRepository;
+import com.cmpe275.CartShare.model.*;
+import com.cmpe275.CartShare.service.OrderItemsService;
 import com.cmpe275.CartShare.service.OrderService;
 import com.cmpe275.CartShare.service.UserService;
 import net.minidev.json.JSONArray;
@@ -33,10 +33,51 @@ public class PickupController {
         return modelAndView;
     }*/
 
+    @Autowired
+    LinkedOrdersRepository linkedOrdersRepository;
+    @Autowired
+    OrderItemsService orderItemsService;
+
     @GetMapping("/pickup/order/{order_id}")
     public ModelAndView pickupOrderView(ModelAndView modelAndView, @PathVariable(name="order_id") Integer order_id){
 
-        modelAndView.addObject("order_id", order_id);
+        List<LinkedOrders> linkedOrders = new ArrayList<>();
+        JSONArray poolOrderList = new JSONArray();
+
+        linkedOrders=linkedOrdersRepository.findAllByParent_id(order_id);
+
+        for(LinkedOrders lo : linkedOrders){
+            System.out.println(lo.getId());
+            JSONObject linkedOrder = new JSONObject();
+            if(lo.getPool_order()!=null && lo.getPool_order().getStatus()!=null && lo.getPool_order().getStatus().equals("PLACED")){
+                JSONArray orderItemsPerPoolOrder = new JSONArray();
+
+                List<OrderItems> orderItems =orderItemsService.getOrderItems(lo.getPool_order());
+                //add orderitems to order
+                for(OrderItems oi : orderItems){
+                    JSONObject orderitem = new JSONObject();
+                    JSONObject product = new JSONObject();
+                    if(oi.getProduct()!=null){
+                        product.put("name",oi.getProduct().getName());
+                        product.put("desc",oi.getProduct().getDescription());
+                        product.put("storeid",oi.getProduct().getStoreid());
+                        product.put("sku",oi.getProduct().getSku());
+                        product.put("unit",oi.getProduct().getUnit());
+                        product.put("brand",oi.getProduct().getBrand());
+                    }
+                    orderitem.put("product",product);
+                    orderitem.put("price",oi.getPrice());
+                    orderitem.put("id",oi.getId());
+                    orderItemsPerPoolOrder.add(orderitem);
+                }
+                linkedOrder.put("orderitems",orderItemsPerPoolOrder);
+                poolOrderList.add(linkedOrder);
+            }
+
+        }
+        //System.out.println(poolOrderList);
+
+        modelAndView.addObject("poolOrderList", poolOrderList);
         modelAndView.setViewName("pickup/view");
         return modelAndView;
     }
