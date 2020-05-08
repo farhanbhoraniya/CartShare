0c5adaf49c9ce5e852255afe690c9177aff9d32b
Shraddha Nayak <shraddhanayak07@gmail.com>
2020-05-07 18:22:59 -0700

    Update placeOrderConfirm.js

--- src/main/resources/static/js/custom/placeOrderConfirm.js
+++ src/main/resources/static/js/custom/placeOrderConfirm.js
@@ -5,6 +5,50 @@ $(document).ready(function(){
 	} 
 	
 	$("#pickOrder").on("click",function(){
-		alert("Clicked");	
+		var data = {
+			"selfPick": false
+		};
+		$.ajax({
+			url: "/order/place",
+			type: "POST",
+			data: JSON.stringify(data),
+			contentType: "application/json",
+			success: function(req){
+				if(req.status === "PLACED"){
+					showNotification("Order Placed successfully",'bg-green','bottom','right');
+					location.reload(true);
+				}
+			},
+			error: function(req){
+				console.log(req);
+			},
+			failure: function(req){
+				console.log(req);
+			}
+		});
+	});
+		
+	$("#notpickOrder").on("click",function(){
+			var data = {
+				"selfPick": true
+			};
+			$.ajax({
+				url: "/order/place",
+				type: "POST",
+				data: JSON.stringify(data),
+				contentType: "application/json",
+				success: function(req){
+					if(req.status === "PLACED"){
+						showNotification("Order Placed successfully",'bg-green','bottom','right');
+						location.reload(true);
+					}
+				},
+				error: function(req){
+					console.log(req);
+				},
+				failure: function(req){
+					console.log(req);
+				}
+			});
 	});
 });
