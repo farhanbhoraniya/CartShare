01122b76c7f501b22104415f65ded6574faf7b89
Farhan <farhanbhoraniya@gmail.com>
2020-05-19 17:15:41 -0700

    Added basic test cases for product and store

--- src/test/java/com/cmpe275/CartShare/CartShareApplicationTests.java
+++ src/test/java/com/cmpe275/CartShare/CartShareApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cmpe275.CartShare;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +10,118 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.cmpe275.CartShare.contollers.AuthController;
+import com.cmpe275.CartShare.contollers.CartContoller;
+import com.cmpe275.CartShare.contollers.DeliveryController;
+import com.cmpe275.CartShare.contollers.MyOrdersController;
+import com.cmpe275.CartShare.contollers.OrderController;
+import com.cmpe275.CartShare.contollers.PickupController;
+import com.cmpe275.CartShare.contollers.PoolController;
+import com.cmpe275.CartShare.contollers.PoolMembershipController;
+import com.cmpe275.CartShare.contollers.ProductController;
+import com.cmpe275.CartShare.contollers.StoreController;
+import com.cmpe275.CartShare.contollers.UserAddressController;
+import com.cmpe275.CartShare.contollers.UserController;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class CartShareApplicationTests {
     
     @Autowired
-    private MockMvc mockMvc;
+    private UserController userController;
+    
+    @Autowired
+    private AuthController authController;
+    
+    @Autowired
+    private CartContoller cartContoller;
+    
+    @Autowired
+    private DeliveryController deliveryController;
+    
+    @Autowired
+    private MyOrdersController myOrdersController;
+    
+    @Autowired
+    private OrderController orderController;
+    
+    @Autowired
+    private PickupController pickupController;
+    
+    @Autowired
+    private PoolController poolController;
+    
+    @Autowired
+    private PoolMembershipController poolMembershipController;
+    
+    @Autowired
+    private ProductController productController;
+    
+    @Autowired
+    private StoreController storeController;
+    
+    @Autowired
+    private UserAddressController userAddressController;
 
     @Test
-    void updateItemInCartTest() throws Exception {
-//        mockMvc.perform(post("/updateItemInCart")
-//                .con)
+    void authController() throws Exception {
+    	assertNotNull(authController);
+    }
+    
+    @Test
+    void cartContoller() throws Exception {
+    	assertNotNull(cartContoller);
+    }
+    
+    @Test
+    void myOrdersController() throws Exception {
+    	assertNotNull(myOrdersController);
+    }
+    
+    @Test
+    void deliveryController() throws Exception {
+    	assertNotNull(deliveryController);
+    }
+    
+    @Test
+    void orderController() throws Exception {
+    	assertNotNull(orderController);
+    }
+    
+    @Test
+    void pickupController() throws Exception {
+    	assertNotNull(pickupController);
+    }
+    
+    @Test
+    void poolController() throws Exception {
+    	assertNotNull(poolController);
+    }
+    
+    @Test
+    void poolMembershipController() throws Exception {
+    	assertNotNull(poolMembershipController);
+    }
+    
+    @Test
+    void productController() throws Exception {
+    	assertNotNull(productController);
+    }
+    
+    @Test
+    void storeController() throws Exception {
+    	assertNotNull(storeController);
+    }
+    
+    @Test
+    void userAddressController() throws Exception {
+    	assertNotNull(userAddressController);
+    }
+    
+    @Test
+    void userController() throws Exception {
+    	assertNotNull(userController);
     }
 
 }
---
+++ src/test/java/com/cmpe275/CartShare/StoreServiceTest.java
@@ -0,0 +1,55 @@
+package com.cmpe275.CartShare;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cmpe275.CartShare.dao.StoreRepository;
+import com.cmpe275.CartShare.model.Store;
+import com.cmpe275.CartShare.service.StoreService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StoreServiceTest { 
+	
+	@Autowired
+	private StoreService storeService;
+	
+	@MockBean
+	private StoreRepository storeRepository;
+	
+	Store store = new Store("test", "street", "number", "city", "zip", "state");
+	
+	@Test
+	public void findStoreById() {
+		Mockito.when(storeRepository.findById(1)).thenReturn(store);
+		Store storeNew = storeService.findById(1);
+		
+		assertEquals(store.getName(), "test");
+	}
+	
+	@Test
+	public void saveStore() {
+		Mockito.when(storeRepository.save(Mockito.any())).thenReturn(store);
+		Mockito.when(storeRepository.findByName(store.getName())).thenReturn(store);
+		Store storeNew = storeService.save(new Store("test", "street", "number", "city", "zip", "state"));
+		
+		assertEquals(store.getName(), "test");
+	}
+	
+	@Test
+	public void deleteStore() {
+		System.out.println("before");
+		Mockito.when(storeRepository.deleteById(1)).thenReturn(store);
+		Store storeNew = storeService.delete(1);
+		
+		assertEquals(store.getName(), "test");
+	}
+
+}
